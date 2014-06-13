0bfa20d77bb821ca3b63cfb485176a7cc0f365bf
run00 <setup@run00.com>
2014-06-13 09:24:15 -0500

    Updating missing items

---
+++ GitWorkItems/GitHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.TeamFoundation.Controls;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Run00.GitWorkItems
+{
+	public class GitHub : IGitHub
+	{
+		public GitHub(IServiceProvider serviceProvider)
+		{
+			_serviceProvider = serviceProvider;
+		}
+
+		private readonly IServiceProvider _serviceProvider;
+	}
+}
---
+++ GitWorkItems/IGitHub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Run00.GitWorkItems
+{
+	public interface IGitHub
+	{
+
+	}
+}
